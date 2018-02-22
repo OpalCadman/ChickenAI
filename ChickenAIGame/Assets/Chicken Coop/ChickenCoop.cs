ad92f8424c05ac64474bc5f9ee78ee2d613fad14
Ben <q5108513@live.tees.ac.uk>
2018-02-22 20:36:36 +0000

    Day Manager
    Began day manager class. Just tracks the current day and has a function
    to advance the day by one.

--- ChickenAIGame/Assets/Chicken Coop/ChickenCoop.cs
+++ ChickenAIGame/Assets/Chicken Coop/ChickenCoop.cs
@@ -7,6 +7,9 @@ public class ChickenCoop : MonoBehaviour {
     //This just keeps track of how many chickens we have generated and is used when assigning the 
     //chickens a unique ID.
 
+    private DayManager dayManager = DayManager.Instance();
+    //Keep a pointer to our dayManager instance for use in this class. Saves us from writing DayManager.Instance(). all the time.
+
     public Dictionary<int, PlayerChicken> playerChickens = new Dictionary<int, PlayerChicken>();
 
     public void Start() {
@@ -18,12 +21,12 @@ public class ChickenCoop : MonoBehaviour {
             //Add the chickens to the map giving them a unique ID for when we need to retrieve data
             //from a specific chicken.
         }
-        Debug.Log("Chicken Count: " + chickenCount);
-        Debug.Log("Chicken 1 Strength: " + playerChickens[0].strengthStat);
-        Debug.Log("Chicken 2 Strength: " + playerChickens[1].strengthStat);
-        Debug.Log("Chicken 3 Strength: " + playerChickens[2].strengthStat);
-        Debug.Log("Chicken 4 Strength: " + playerChickens[3].strengthStat);
-        Debug.Log("Chicken 5 Strength: " + playerChickens[4].strengthStat);
-        Debug.Log("Chicken 6 Strength: " + playerChickens[5].strengthStat);
+
+        Debug.Log(dayManager.currentDay);
+        
+        for(int i = 0; i < 10; i++) {
+            dayManager.AdvanceDay();
+            Debug.Log(dayManager.currentDay);
+        }
     }
 }
---
+++ ChickenAIGame/Assets/Chicken Coop/DayManager.cs
@@ -0,0 +1,28 @@
+﻿
+public class DayManager {
+    
+    public enum Day {
+        Monday = 0,
+        Tuesday = 1,
+        Wednesday = 2,
+        Thursday = 3,
+        Friday = 4,
+        Saturday = 5,
+        Sunday = 6
+    }
+
+    public Day currentDay;
+    public int noOfDaysPast;
+
+	public void AdvanceDay() {
+        if((int)currentDay != 6) {
+            currentDay++;
+        } else {
+            currentDay = 0;
+        }
+    }
+
+    static public DayManager Instance() { return dayManager; }
+    static private DayManager dayManager = new DayManager();
+    protected DayManager() { }
+}
