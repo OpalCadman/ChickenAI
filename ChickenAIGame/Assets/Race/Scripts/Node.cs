338533099063fc39fb9052f139383fc818eb7053
OpalCadman <little_opal@hotmail.co.uk>
2018-03-07 21:36:10 +0000

    Started Pathfinding

---
+++ ChickenAIGame/Assets/Race/Scripts/Node.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Created node class for A* Pathfinding. These will be used to create a grid of nodes to allow them to be 
+into consideration when creating the grid and pathfinding. We need to know if you can walk on it to 
+distinguish whether or not it is an obstacle and we need to know where it is in the world.*/
+public class Node
+{
+    public bool canWalk;
+    public Vector3 WorldPos;
+
+    public Node(bool can_walk, Vector3 World_Pos)
+    {
+        canWalk = can_walk;
+        WorldPos = World_Pos;
+    }
+}
---
+++ ChickenAIGame/Assets/Race/Scripts/Pathfinding_Grid.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding_Grid : MonoBehaviour {
+
+    public LayerMask canWalkMask;
+    public Vector2 G_worldSize;
+    public float N_Radius;
+    Node[,] grid;
+
+
+    private void OnDrawGizmos()
+    {
+    }
+}
