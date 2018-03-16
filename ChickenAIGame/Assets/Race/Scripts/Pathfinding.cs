fd0e6a298c4ea6e5f346ecde0898b5ae2b78e57f
OpalCadman <little_opal@hotmail.co.uk>
2018-03-16 20:21:45 +0000

    Started pathfinding

--- ChickenAIGame/Assets/Race/Scripts/Pathfinding.cs
+++ ChickenAIGame/Assets/Race/Scripts/Pathfinding.cs
@@ -21,5 +21,27 @@ public class Pathfinding : MonoBehaviour
         HashSet<Node> closed_set = new HashSet<Node>();
 
         open_Set.Add(start_Node);
+
+        while (open_Set.Count > 0)
+        {
+            Node cur_node = open_Set[0];
+            for (int i = 1; i < open_Set.Count; ++i)
+            {
+                if (open_Set[i].F_Cost < cur_node.F_Cost || open_Set[i].F_Cost == cur_node.F_Cost && open_Set[i].H_Cost < cur_node.H_Cost)
+                {
+                    cur_node = open_Set[i];
+                }
+            }
+
+            open_Set.Remove(cur_node);
+            closed_set.Add(cur_node);
+
+            if (cur_node == target_Node)
+            {
+                return;
+            }
+
+            /*MINUTE 7:52 ON VIDEO*/
+        }
     }
 }
