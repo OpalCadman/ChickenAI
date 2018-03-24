24fe0d31c05a6261bf305caa53064f6619908ffe
OpalCadman <little_opal@hotmail.co.uk>
2018-03-24 12:41:30 +0000

    Heap added for optimisation. Now starting to add units in and what not. Path is being found

--- ChickenAIGame/Assets/Race/Scripts/Node.cs
+++ ChickenAIGame/Assets/Race/Scripts/Node.cs
@@ -5,23 +5,55 @@ using UnityEngine;
 /*Created node class for A* Pathfinding. These will be used to create a grid of nodes to allow them to be 
 into consideration when creating the grid and pathfinding. We need to know if you can walk on it to 
 distinguish whether or not it is an obstacle and we need to know where it is in the world.*/
-public class Node
+public class Node : Iheap_Item<Node>
 {
     public bool canWalk;
     public Vector3 WorldPos;
+    public int X_Grid;
+    public int Y_Grid;
+
     public int G_Cost;
     public int H_Cost;
+    public Node parent;
+    int heap_Ind;
 
-    public Node(bool can_walk, Vector3 World_Pos)
+    public Node(bool can_walk, Vector3 World_Pos, int _XGrid, int _YGrid)
     {
         canWalk = can_walk;
         WorldPos = World_Pos;
+        X_Grid = _XGrid;
+        Y_Grid = _YGrid;
+
     }
 
     public int F_Cost
     {
-        get {
+        get
+        {
             return G_Cost + H_Cost;
         }
+    }
+
+    public int Index_Heap
+    {
+        get
+        {
+            return heap_Ind;
+        }
+
+        set
+        {
+            heap_Ind = value;
+        }
+    }
+
+    public int CompareTo(Node Compare_Node)
+    {
+        int compare = F_Cost.CompareTo(Compare_Node.F_Cost);
+        if (compare == 0)
+        {
+            compare = H_Cost.CompareTo(Compare_Node.H_Cost);
+        }
+        return -compare;
     }
 }
---
+++ ChickenAIGame/Assets/Race/Scripts/RequestPathManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class RequestPathManager : MonoBehaviour
+{
+    Queue<RequestPath> QueueRequestPath = new Queue<RequestPath>();
+    RequestPath currentRequestPath;
+
+    static RequestPathManager instance;
+
+
+    void Awake()
+    {
+        instance = this;   
+    }
+    public static void Path_Request(Vector3 Start_Path, Vector3 End_Path, Action<Vector3[], bool> waitingTime)
+    {
+        RequestPath RequestNew = new RequestPath(Start_Path, End_Path, waitingTime);
+        instance.QueueRequestPath.Enqueue(RequestNew);
+        instance.TryNextProcess();
+    }
+
+    void TryNextProcess()
+    {
+        /*5:51 Units episode 6*/
+    }
+
+    struct RequestPath
+    {
+        public Vector3 startPath;
+        public Vector3 endPath;
+        Action<Vector3[], bool> waitingTime;
+
+        public RequestPath(Vector3 _startPath, Vector3 _endPath, Action<Vector3[], bool> _waitingTime)
+        {
+            startPath = _startPath;
+            endPath = _endPath;
+            waitingTime = _waitingTime;
+
+        }
+    }
+
+}
