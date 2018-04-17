ea07ff7866f796df573f672a61bb0ec62da6be6e
LiquidAkiba <akiba6689@gmail.com>
2018-04-17 16:21:16 +0100

    Decision Making Scripts Added

---
+++ ChickenAIGame/Assets/Decision Making/DecisionNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecisionNode : MonoBehaviour
+{
+
+    public string testValue;
+    public Dictionary<float, DecisionNode> children;
+
+    public DecisionNode(string testValue = "")
+    {
+        this.testValue = testValue;
+        children = new Dictionary<float, DecisionNode>();
+    }
+    
+
+}
+
+	
---
+++ ChickenAIGame/Assets/Decision Making/ID3Example.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ID3Action
+// All possible actions for the chickens to perform.
+{
+    STOP, WALK, RUN, JUMP
+}
+
+
+public class ID3Example : MonoBehaviour
+{
+    public ID3Action action;
+    public Dictionary<string, float> values;
+
+    public float GetValue(string attribute)
+    {
+        return values[attribute];
+    }
+}
+
