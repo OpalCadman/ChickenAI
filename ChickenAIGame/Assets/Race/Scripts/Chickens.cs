9cf42d8f4bfa107f5dd318b94e83cbf965a5981e
OpalCadman <little_opal@hotmail.co.uk>
2018-04-27 18:39:39 +0100

    Added Art and Sound

--- ChickenAIGame/Assets/Race/Scripts/Chickens.cs
+++ ChickenAIGame/Assets/Race/Scripts/Chickens.cs
@@ -6,7 +6,7 @@ public class Chickens : MonoBehaviour
 {
     public Transform target;
     public GameObject race;
-    float speed = 10f;
+    float speed = 4f;
     Vector3[] path;
     int target_index;
     public bool reset = false;
